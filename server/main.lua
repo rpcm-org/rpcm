d58763e11935a6b65a3b5e3a6defe6566fb2745c
celestian <petr.celestian@gmail.com>
2018-11-18 07:47:34 +0100

    WiP: Save user

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,45 @@
 RegisterServerEvent('playerSpawned')
+
 AddEventHandler('playerSpawned', function()
-    print("Player " ..source .." spawned!")
-    local identifiers = GetPlayerIdentifiers(source)
-    print("identifiers:")
-    for k, v in pairs(identifiers) do
-        print(k, v)
+
+    print('Lua version: ', _VERSION)
+
+    local ped = source
+    local identifiers = GetPlayerIdentifiers(ped)
+
+    local steam_id = nil
+    local license = nil
+    for key, value in pairs(identifiers) do
+
+        identifier, number = string.match(value, '(.*):(.*)')
+
+        if identifier == 'steam' then
+            steam_id = number
+        end
+
+        if identifier == 'license' then
+            license = number
+        end
     end
+
+    if steam_id and license then
+        print('Player: ' , steam_id, ' | ', license)
+
+        local driver = require"luasql.mysql"
+        local env = driver.mysql()
+        print('>> ', env)
+
+        MySQL.ready(function ()
+            MySQL.Async.fetchAll("SELECT COUNT(id) FROM users WHERE steam_id = @steam_id", {['@steam_id'] = steam_id}, function(rowsChanged)
+                for k, v in pairs(rowsChanged) do
+                    print('>> ', k, ' :: ', v)
+                end
+            end)
+        end)
+
+    else
+        print('Nepovedlo se')
+        -- TODO: Display warning, you don't have steam running or you don't have legal GTA V.
+    end
+
 end)
